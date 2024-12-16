19c7d9e779f20b71ef245c299ac540aaa20a5149
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-12-16 21:39:57 +0700

    feat: add geolocator and some firsthand demonstration

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -8,13 +9,27 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Future<void> getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+      locationSettings: LocationSettings(
+        accuracy: LocationAccuracy.lowest,
+        distanceFilter: 10,
+      ),
+    );
+
+    debugPrint('Latitude: ${position.latitude}');
+    debugPrint('Longitude: ${position.longitude}');
+    debugPrint('Accuracy: ${position.accuracy}');
+    debugPrint('Timestamp: ${position.timestamp}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
